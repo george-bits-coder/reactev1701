0581d0935545366bdc0739fb6c2076bbcd67525f
George Abraham <91771684+george-bits-coder@users.noreply.github.com>
2022-01-17 20:42:39 +0530

    Update reducer.js

--- src/Features/User/reducer.js
+++ src/Features/User/reducer.js
@@ -3,12 +3,13 @@ import { APPLY } from "./actionTypes";
 const jobd = { names: [], jobs: [] };
 
 export const reducer = (state = jobd, { type, payload }) => {
+  console.log(payload);
   switch (type) {
     case APPLY:
       return {
         ...state,
-        names: [...state.names, payload[n]],
-        jobs: [...state.jobs, payload[j]]
+        names: [...state.names, payload.n],
+        jobs: [...state.jobs, payload.j]
       };
 
     default:
